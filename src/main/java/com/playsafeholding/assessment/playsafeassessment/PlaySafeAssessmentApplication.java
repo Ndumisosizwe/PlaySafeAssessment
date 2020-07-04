4aa3c7df4378b54da050e212112f5ed5da94d485
Ndumisosizwe <ndumisosizwe@gmail.com>
2020-07-04 12:08:29 +0200

    Spring Initializer Project - created new project

---
+++ src/main/java/com/playsafeholding/assessment/playsafeassessment/PlaySafeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.playsafeholding.assessment.playsafeassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaySafeAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaySafeAssessmentApplication.class, args);
+    }
+
+}
