88664a2a5c537a89e25cf56e85766fea29b072d7
Ndumisosizwe <ndumisosizwe@gmail.com>
2020-07-04 12:19:34 +0200

    Spring Initializer Project - config mysql driver - changed base package name

--- src/main/java/com/playsafeholding/assessment/PlaySafeAssessmentApplication.java
+++ src/main/java/com/playsafeholding/assessment/PlaySafeAssessmentApplication.java
@@ -1,4 +1,4 @@
-package com.playsafeholding.assessment.playsafeassessment;
+package com.playsafeholding.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/playsafeholding/assessment/PlaySafeAssessmentApplicationTests.java
+++ src/test/java/com/playsafeholding/assessment/PlaySafeAssessmentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.playsafeholding.assessment.playsafeassessment;
+package com.playsafeholding.assessment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
